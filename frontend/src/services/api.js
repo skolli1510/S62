8b1307c68c2c286d15a5ca7af92bf8cb2efe24b4
skolli1510 <skolli8@charlotte.edu>
2024-11-20 14:22:36 -0500

    Update api.js

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
 export const login = async (credentials) => {
-    const response = await axios.post('http://localhost:5000/api/login', credentials);
+    const response = await axios.post('https://s62.onrender.com/api/login', credentials);
   return response.data;
 };
